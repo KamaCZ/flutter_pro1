fa0ad6cf94a5187382465afa4cfdf87a246e2b5d
KamaCZ <kama.klemsa@gmail.com>
2021-11-05 10:18:15 +0100

    day6

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,26 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What\s your favorite animal?"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("FRU HAUGANS HOTEL"),
         ),
-        body:
-            Text("Informations about the norwegian hotel and restaurant chain"),
+        body: Column(
+          children: [
+            Text("Choose the location"),
+            RaisedButton(child: Text("Fru Haugans Hotel"), onPressed: null),
+            RaisedButton(child: Text("Froken Skjold Hotel"), onPressed: null),
+            RaisedButton(
+                child: Text("Blomsterbua Mat og Vin"), onPressed: null),
+            RaisedButton(child: Text("Olympen"), onPressed: null),
+            RaisedButton(child: Text("Pigallo"), onPressed: null),
+          ],
+        ),
       ),
     );
   }
